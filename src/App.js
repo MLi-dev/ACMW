9f30428f5e083339f8c38d073cfc93332d65b261
kylipoo <kyleyli2005@gmail.com>
2022-10-25 21:52:37 -0700

    Testing Commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+constructor() {
+  super();
+
+  this.state = {
+  monsters: [
+   {
+    name: 'Billy',
+    id: '1234',
+  }, 
+   {
+    name: 'Frank',
+    id: '4567',
+  }, 
+   {
+    name: 'Jacky',
+    id: '89101',
+  }, 
+  {
+    name: 'Andrei',
+    id: '4325',
+  }, 
+]
+  };
+}
+
+  render() {
+  return (
+    <div className="App">
+     {
+      this.state.monsters.map((monster) =>{
+        return <div key={monster.id}><h1>{monster.name}</h1></div>
+      })
+     }
+    </div>
+  );
+}
+}
+export default App;
