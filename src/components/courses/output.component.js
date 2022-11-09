74aba8b7da4a011cb8fc091b9bda3bbe0193ac79
Matthew Li <matthewli2003@gmail.com>
2022-11-09 09:38:40 -0800

    Implemented video player

--- src/components/courses/output.component.js
+++ src/components/courses/output.component.js
@@ -1,11 +1,11 @@
+import Courses from "./courses.component";
 import "./courses.styles.css";
-import story from "./output.js"; 
+import course from "./output.js";
 
 const Output = ({id}) => {
     return (
         <div className = "outputContainer">
-        <div dangerouslySetInnerHTML={{ __html: story(id) }}/>
-        <iframe title = "Course sample" width = "420" height = "315" src = "https://www.youtube.com/watch?v=zg2zb6h-oN0"></iframe>
+        <iframe title = "Example" width="420" height="315" src={course(id)} frameborder="0" allowfullscreen></iframe>
         </div>
     );
 };
--- src/components/courses/output.js
+++ src/components/courses/output.js
@@ -1,15 +1,15 @@
-const story = (id) => {
+const course = (id) => {
     let resp = "";
     switch(id) {
-        case 1: resp = 'Hello, I am Matthew Li'
+        case 1: resp = 'https://www.youtube.com/embed/XmkGxHZDer8'
         break; 
-        case 2: resp = 'You are, come again?'
+        case 2: resp = 'https://www.youtube.com/embed/FazgJVnrVuI'
         break; 
-        case 3: resp = 'Bye'
+        case 3: resp = 'https://www.youtube.com/embed/FWTNMzK9vG4'
         break; 
         default: 
         break; 
     }
     return resp; 
 }
-export default story;   
+export default course;   
