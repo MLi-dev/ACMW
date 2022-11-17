501ec428bb56694f8dd999087de8dd64e80335e6
Matthew Li <matthewli2003@gmail.com>
2022-11-17 13:30:19 -0800

    Added default image, changed margins.

--- src/components/courses/output.component.js
+++ src/components/courses/output.component.js
@@ -1,5 +1,5 @@
 import "./courses.styles.css";
-import "./output.styles.css"
+import "./output.styles.css";
 import Quiz from "../quiz/quiz.component";
 
 const Output = ({ id, type, category, video }) => {
@@ -25,9 +25,20 @@ const Output = ({ id, type, category, video }) => {
 	} else
 		return (
 			<div id='courseDisplayContainer'>
-				<span className="welcome">Welcome to your {category} Learning Journey, please select a course or
-                take a quiz
+				<span className='welcome'>
+					Welcome to your {category} Learning Journey, please select a course or
+					take a quiz
                 </span>
+                <div className = "courseImage">
+				<img
+					alt=''
+					width='750'
+					height='422'
+					class='single-course-unit--course-image--XinHF'
+					src='https://img-c.udemycdn.com/course/480x270/4015622_2fee_4.jpg'
+					srcset='https://img-c.udemycdn.com/course/480x270/4015622_2fee_4.jpg 1x, https://img-c.udemycdn.com/course/750x422/4015622_2fee_4.jpg 2x'
+                ></img>
+                </div>
 			</div>
 		);
 };
