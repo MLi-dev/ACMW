98101f87d8fe89523e566d384ae8ed10ff07eaaa
Matthew Li <matthewli2003@gmail.com>
2022-11-23 14:33:32 -0800

    Added Sign up link in homepage.

--- src/home/home.component.jsx
+++ src/home/home.component.jsx
@@ -1,37 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import CardList from '../components/card-list/card-list.component';
-import SearchBox from '../components/search-box/search-box.component'
-import {categories as monsters} from './categories';
+import CardList from "../components/card-list/card-list.component";
+import SearchBox from "../components/search-box/search-box.component";
+import { categories as monsters } from "./categories";
+import { Link } from "react-router-dom";
+import "./home.styles.css";
 const Home = () => {
-  const [searchField, setSearchField] = useState('');
-  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+	const [searchField, setSearchField] = useState("");
+	const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-  useEffect(() => {
-    const newFilteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
+	useEffect(() => {
+		const newFilteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
+		});
 
-    setFilterMonsters(newFilteredMonsters);
-  }, [searchField]);
+		setFilterMonsters(newFilteredMonsters);
+	}, [searchField]);
 
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
-  };
+	const onSearchChange = (event) => {
+		const searchFieldString = event.target.value.toLocaleLowerCase();
+		setSearchField(searchFieldString);
+	};
 
-  return (
-    <div className='App'>
-      <h1 className='app-title'>ACMW Learning Resources</h1>
+	return (
+		<div className='App'>
+			<div className='susi-container'>
+				<Link to={`/SignUp`}>
+					<h2>Sign up</h2>
+				</Link>
+			</div>
+			<h1 className='app-title'>ACMW Learning Resources</h1>
 
-      <SearchBox
-        className='monsters-search-box'
-        onChangeHandler={onSearchChange}
-        placeholder='search monsters'
-      />
-      <CardList monsters={filteredMonsters} />
-    </div>
-  );
+			<SearchBox
+				className='monsters-search-box'
+				onChangeHandler={onSearchChange}
+				placeholder='search monsters'
+			/>
+			<CardList monsters={filteredMonsters} />
+		</div>
+	);
 };
 
 export default Home;
