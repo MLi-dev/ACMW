9d25b162c8a621b35665434cd8f830d3f0c87c40
Matthew Li <matthewli2003@gmail.com>
2022-11-29 17:17:38 -0800

    Removed scrollbar from courses

--- src/components/courses/courses.component.jsx
+++ src/components/courses/courses.component.jsx
@@ -62,7 +62,6 @@ const Courses = ({ id }) => {
 			</header>
 			<section>
 				<nav>
-					<div className='courses-container'>
 						<ul id='courseMenu'>
 							{error && <div>No Courses/Video available!</div>}
 							{pending && <div>Loading!</div>}
@@ -96,7 +95,6 @@ const Courses = ({ id }) => {
 								</a>
 							</li>
 						</ul>
-					</div>
 				</nav>
 				<article>
 					<Output id={courseId} type={type} category={category} video={video} />
